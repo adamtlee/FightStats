7079ac00fa629de6ac29ec8fc2a5d74d75747ba4
adamtlee <adam.lee346@gmail.com>
2021-10-24 15:50:24 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace fightstats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Path.Combine(Environment.CurrentDirectory, @"data\", "fightstatsdata.csv");
+
+            try
+            {
+                if (File.Exists(path))
+                {
+                    Console.WriteLine("File exists"); 
+                }
+
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    while (sr.Peek() >= 0)
+                    {
+                        Console.WriteLine(sr.ReadLine());
+                    }
+                }
+            } 
+            catch (Exception e)
+            {
+                Console.WriteLine("The process failed: {0}", e.ToString());
+            }
+        }
+    }
+}
