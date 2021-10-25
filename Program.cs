a3fa82afa8d62832924d61bc4ec035a6123b22ae
adamtlee <adam.lee346@gmail.com>
2021-10-24 23:35:57 -0600

    added athlete model

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 
@@ -9,7 +10,33 @@ namespace fightstats
         static void Main(string[] args)
         {
             string path = Path.Combine(Environment.CurrentDirectory, @"data\", "fightstatsdata.csv");
+            
+            var Athletes = new List<Athlete>()
+            {
+                new Athlete(){
+                    Team = "Independent",
+                    Name = "Bruce Lee",
+                    Win = 100,
+                    Loss = 0,
+                    Draw = 0,
+                    NoContest = 0
+                    
+                }
+            };
 
+            Console.WriteLine("Athlete in the List");
+           
+           foreach (Athlete athlete in Athletes)
+           {
+               Console.WriteLine("Team: " + athlete.Team);
+               Console.WriteLine("Name: " + athlete.Name);
+               Console.WriteLine("Win: " + athlete.Win);
+               Console.WriteLine("Loss: " + athlete.Loss);
+               Console.WriteLine("Draw: " + athlete.Draw);
+               Console.WriteLine("No Contest: " + athlete.NoContest);
+           }
+            
+            Console.WriteLine();
             try
             {
                 if (File.Exists(path))
@@ -19,9 +46,11 @@ namespace fightstats
 
                 using (StreamReader sr = new StreamReader(path))
                 {
+                    Console.WriteLine("Athletes in the CSV File: ");
                     while (sr.Peek() >= 0)
                     {
                         Console.WriteLine(sr.ReadLine());
+
                     }
                 }
             } 
---
+++ models/athlete.cs
@@ -0,0 +1,11 @@
+namespace fightstats
+{
+    public class Athlete{
+        public string Team {get; set;}
+        public string Name {get; set;}
+        public int Win {get; set;}
+        public int Loss {get; set;}
+        public int Draw {get; set;}
+        public int NoContest {get; set;}
+    }
+}
